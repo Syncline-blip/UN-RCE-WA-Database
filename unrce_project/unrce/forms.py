fccd93a480061cef31a45fa174e88a293464d2b6
J Lumagbas <95333229+Syncline-blip@users.noreply.github.com>
2023-08-22 16:34:06 +0800

    set up model for user form

--- unrce_project/unrce/forms.py
+++ unrce_project/unrce/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Report
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ReportForm(forms.ModelForm):
     class Meta:
@@ -15,3 +17,15 @@ class ReportForm(forms.ModelForm):
             'sdg_focus', 
             'contact',
         ]
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2", "name", "lastName", "org") 
+
+        def save(self, commit=True):
+            user = super(RegistrationForm, self).save(commit=False)
+            user.email = self.cleaned_data['email']
+            if commit: user.save()
+            return user
+
