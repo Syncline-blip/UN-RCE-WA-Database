aeb399502b8846f7e3c73161645c38242ef013fb
J Lumagbas <95333229+Syncline-blip@users.noreply.github.com>
2023-08-23 14:08:23 +0800

    removed crispy forms

--- unrce_project/unrce/forms.py
+++ unrce_project/unrce/forms.py
@@ -2,8 +2,7 @@ from .models import Report, Account
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit
+
 
 class ReportForm(forms.ModelForm):
     class Meta:
