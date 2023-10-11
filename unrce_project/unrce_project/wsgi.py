c3f7be4a69c7f0618e7136a115b2c973aa756a94
J Lumagbas <jadlumagbas@gmail.com>
2023-10-11 22:44:51 +0800

    pls

--- unrce_project/unrce_project/wsgi.py
+++ unrce_project/unrce_project/wsgi.py
@@ -10,12 +10,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
+# Assuming wsgi.py is in the 'unrce_project' directory
 root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add the parent directory to sys.path
 sys.path.append(root_path)
 
-# Add the subdirectories as needed
-sys.path.append(os.path.join(root_path, 'unrce_project'))
-sys.path.append(os.path.join(root_path, 'unrce_project', 'unrce'))
+# Set the DJANGO_SETTINGS_MODULE
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unrce_project.settings")
 
+# Create the WSGI application
 application = get_wsgi_application()
