4211df485f5ce1af1aedbf6a02314554848b14e4
J Lumagbas <jadlumagbas@gmail.com>
2023-10-11 23:10:02 +0800

    www

--- unrce_project/unrce_project/settings.py
+++ unrce_project/unrce_project/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'unrce_project.unrce',
+
 ]
 
 AUTHENTICATION_BACKENDS = [
