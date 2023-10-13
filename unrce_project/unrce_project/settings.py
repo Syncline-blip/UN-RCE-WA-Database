35fb1521c98767f8133d490de7b6a425808e09c3
J Lumagbas <95333229+Syncline-blip@users.noreply.github.com>
2023-10-13 11:53:44 +0800

    change

--- unrce_project/unrce_project/settings.py
+++ unrce_project/unrce_project/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -157,7 +157,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # Add this line
 
 ]
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
