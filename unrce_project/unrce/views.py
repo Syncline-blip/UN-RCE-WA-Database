886f2bda8c291948bb9c52007830bfa44ad53f5e
Sloth9731 <samweight@icloud.com>
2023-10-15 21:35:36 +0800

    changes to profile

--- unrce_project/unrce/views.py
+++ unrce_project/unrce/views.py
@@ -25,6 +25,8 @@ from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+
 
 from django.contrib.auth.forms import PasswordChangeForm
 
