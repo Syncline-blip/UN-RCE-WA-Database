ef89e96734b47ddc68c5a182631f7d51f79b73af
J Lumagbas <95333229+Syncline-blip@users.noreply.github.com>
2023-10-16 14:38:43 +0800

    2222

--- unrce_project/wsgi.py
+++ unrce_project/wsgi.py
@@ -14,8 +14,7 @@ root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root_path)
 
 # Add the subdirectories as needed
-sys.path.append(os.path.join(root_path, 'unrce_project'))
-sys.path.append(os.path.join(root_path, 'unrce_project', 'unrce'))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unrce_project.unrce_project.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unrce_project.settings")
 
 application = get_wsgi_application()
